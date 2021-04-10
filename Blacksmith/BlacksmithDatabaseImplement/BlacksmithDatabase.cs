ef30c4914c9c45e11acced7c58c33b34730fea59
Liza_Voronkova <liza.voronkova.01@mail.ru>
2021-04-10 16:03:19 +0400

    lab 3

---
+++ Blacksmith/BlacksmithDatabaseImplement/BlacksmithDatabase.cs
@@ -0,0 +1,21 @@
+﻿using BlacksmithDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlacksmithDatabaseImplement
+{
+    public class BlacksmithDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-V0A9OFL\SQLEXPRESS;initial catalog=BlacksmithDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Manufacture> Manufactures { set; get; }
+        public virtual DbSet<ManufactureComponent> ManufactureComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
---
+++ Blacksmith/BlacksmithDatabaseImplement/Models/Manufacture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace BlacksmithDatabaseImplement.Models
+{
+    public class Manufacture
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string ManufactureName { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [ForeignKey("ManufactureId")]
+        public virtual List<Order> Orders { get; set; }// odin ko mnogim
+
+        [ForeignKey("ManufactureId")]
+        public virtual List<ManufactureComponent> ManufactureComponents { get; set; } //mnogie ko mnogim
+    }
+}
