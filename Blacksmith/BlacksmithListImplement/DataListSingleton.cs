3ed8a6b5cc96d8e0a31e56a391880fae8e7f7bb0
Liza_Voronkova <liza.voronkova.01@mail.ru>
2021-05-09 01:38:42 +0400

    lab 6 file list implements

--- Blacksmith/BlacksmithListImplement/DataListSingleton.cs
+++ Blacksmith/BlacksmithListImplement/DataListSingleton.cs
@@ -11,12 +11,14 @@ namespace BlacksmithListImplement
         public List<Order> Orders { get; set; }
         public List<Manufacture> Manufactures { get; set; }
         public List<Client> Clients { get; set; }
+        public List<Implementer> Implementers { get; set; }
         private DataListSingleton()
         {
             Components = new List<Component>();
             Orders = new List<Order>();
             Manufactures = new List<Manufacture>();
             Clients = new List<Client>();
+            Implementers = new List<Implementer>();
         }
         public static DataListSingleton GetInstance()
         {
---
+++ Blacksmith/BlacksmithListImplement/Implements/ImplementerStorage.cs
@@ -0,0 +1,118 @@
+﻿using BlacksmithBusinessLogic.BindingModels;
+using BlacksmithBusinessLogic.Interfaces;
+using BlacksmithBusinessLogic.ViewModels;
+using BlacksmithListImplement.Models;
+using System;
+using System.Collections.Generic;
+
+namespace BlacksmithListImplement.Implements
+{
+    public class ImplementerStorage : IImplementerStorage
+    {
+        private readonly DataListSingleton source;
+
+        public ImplementerStorage()
+        {
+            source = DataListSingleton.GetInstance();
+        }
+
+        public void Delete(ImplementerBindingModel model)
+        {
+            foreach (var implementer in source.Implementers)
+            {
+                if (implementer.Id == model.Id)
+                {
+                    source.Implementers.Remove(implementer);
+                }
+            }
+            throw new Exception("Исполнитель не найден");
+        }
+
+        public ImplementerViewModel GetElement(ImplementerBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            foreach (var implementer in source.Implementers)
+            {
+                if (implementer.Id == model.Id)
+                {
+                    return CreateModel(implementer);
+                }
+            }
+            return null;
+        }
+
+        public List<ImplementerViewModel> GetFilteredList(ImplementerBindingModel model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var result = new List<ImplementerViewModel>();
+            foreach (var implementer in source.Implementers)
+            {
+                if (implementer.ImplementerFIO.Contains(model.ImplementerFIO))
+                {
+                    result.Add(CreateModel(implementer));
+                }
+            }
+            return result;
+        }
+
+        public List<ImplementerViewModel> GetFullList()
+        {
+            var result = new List<ImplementerViewModel>();
+            foreach (var implementer in source.Implementers)
+            {
+                result.Add(CreateModel(implementer));
+            }
+            return result;
+        }
+
+        public void Insert(ImplementerBindingModel model)
+        {
+            Implementer tempImplementer = new Implementer { Id = 1 };
+            foreach (var implementer in source.Implementers)
+            {
+                if (implementer.Id >= tempImplementer.Id)
+                {
+                    tempImplementer.Id = implementer.Id + 1;
+                }
+            }
+            source.Implementers.Add(CreateModel(model, tempImplementer));
+        }
+
+        public void Update(ImplementerBindingModel model)
+        {
+            foreach (var implementer in source.Implementers)
+            {
+                if (implementer.Id == model.Id)
+                {
+                    CreateModel(model, implementer);
+                }
+            }
+            throw new Exception("Исполнитель не найден");
+        }
+
+        private Implementer CreateModel(ImplementerBindingModel model, Implementer implementer)
+        {
+            implementer.ImplementerFIO = model.ImplementerFIO;
+            implementer.WorkingTime = model.WorkingTime;
+            implementer.PauseTime = model.PauseTime;
+            return implementer;
+        }
+
+        private ImplementerViewModel CreateModel(Implementer implementer)
+        {
+            return new ImplementerViewModel
+            {
+                Id = implementer.Id,
+                ImplementerFIO = implementer.ImplementerFIO,
+                WorkingTime = implementer.WorkingTime,
+                PauseTime = implementer.PauseTime
+            };
+        }
+    }
+}
